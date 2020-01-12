1cd02002341791b6b2e81a07af039386f0771e79
unknown <mbellman@nwe.com>
2020-01-12 01:05:08 -0600

    Crude lighting

--- adamantine/shaders/l-fragment.glsl
+++ adamantine/shaders/l-fragment.glsl
@@ -9,9 +9,49 @@ in vec2 fragmentUv;
 
 layout (location = 0) out vec3 color;
 
+struct Light {
+  vec3 position;
+  vec3 color;
+  float radius;
+  float intensity;
+};
+
+const Light lights[5] = Light[5](
+  Light(
+    vec3(0.0, 15.0, 250.0),
+    vec3(1.0, 0.0, 1.0),
+    50.0,
+    1.0
+  ),
+  Light(
+    vec3(-100.0, 10.0, 100.0),
+    vec3(1.0, 0.0, 0.0),
+    300.0,
+    1.0
+  ),
+  Light(
+    vec3(200.0, 50.0, -200.0),
+    vec3(0.0, 1.0, 1.0),
+    150.0,
+    1.0
+  ),
+  Light(
+    vec3(-200.0, 15.0, -150.0),
+    vec3(1.0, 1.0, 0.0),
+    50.0,
+    1.0
+  ),
+  Light(
+    vec3(-100.0, 10.0, 300.0),
+    vec3(0.0, 1.0, 0.0),
+    30.0,
+    1.0
+  )
+);
+
 float getBlurFactor(float depth) {
   float MIN_BLUR = 0.0;
-  float MAX_BLUR = 0.002;
+  float MAX_BLUR = 0.0015;
   float MAX_BLUR_DEPTH = 300.0;
 
   float r = depth / MAX_BLUR_DEPTH;
@@ -40,9 +80,36 @@ vec4 getDoF() {
 }
 
 void main() {
+  float ambientLightFactor = 0.01;
+  vec3 albedo = texture(colorTexture, fragmentUv).xyz;
   vec3 position = texture(positionTexture, fragmentUv).xyz;
-  vec3 relativePosition = position - cameraPosition;
-  float mag = length(relativePosition);
+  vec3 normal = texture(normalDepthTexture, fragmentUv).xyz;
+  vec3 cameraToSurface = position - cameraPosition;
+  vec3 baseColor = albedo * ambientLightFactor;
+  vec3 lightSum = vec3(0.0);
+  int total = 0;
 
-  color = getDoF().xyz * clamp(mix(0.0, 1.0, 1 - mag / 500.0), 0.0, 1.0);
+  for (int i = 0; i < lights.length(); i++) {
+    Light light = lights[i];
+
+    vec3 lightToSurface = position - light.position;
+    float lightToSurfaceDistance = length(lightToSurface);
+    float lightIncidence = dot(normalize(lightToSurface), normal);
+
+    if (lightIncidence < 0) {
+      const float PI = 3.141592;
+      vec3 litColor = mix(albedo, light.color, 0.5);
+      float viewIncidence = dot(normalize(cameraToSurface), normal);
+      float attenuation = pow(1.0 / lightToSurfaceDistance, 1.0 / 10.0);
+      float strength = pow(1.0 - lightToSurfaceDistance / light.radius, 2.0);
+      float illuminance = (1.0 + cos((1.0 + lightIncidence) * PI)) / 2.0;
+      float glance = 1.0 - abs(viewIncidence);
+      float intensity = pow(illuminance, 5) * attenuation * glance * 2.0;
+
+      lightSum += mix(baseColor, litColor, intensity);
+      total++;
+    }
+  }
+
+  color = baseColor + lightSum / total;
 }
