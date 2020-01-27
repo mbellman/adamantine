8a6c85ef3cdf736ee9351b3b582104c078152f0e
unknown <mbellman@nwe.com>
2020-01-26 21:09:58 -0600

    Small lighting optimization

--- adamantine/shaders/lighting.glsl
+++ adamantine/shaders/lighting.glsl
@@ -21,14 +21,15 @@ in vec2 fragmentUv;
 
 layout (location = 0) out vec4 color;
 
-vec3 getAmbientLightFactor(vec3 surfacePosition, vec3 surfaceNormal) {
+vec3 getAmbientLightFactor(vec3 surfacePosition, vec3 surfaceNormal, vec3 surfaceToCamera) {
   vec3 surfaceToLight = -1.0 * normalize(ambientLightDirection);
-  vec3 surfaceToCamera = cameraPosition - surfacePosition;
 
+  // Diffuse term
   float normalDot = dot(surfaceToLight, surfaceNormal);
   vec3 diffuse = ambientLightColor * max(normalDot, 0.0);
 
-  vec3 halfVector = normalize(normalize(surfaceToCamera) + normalize(surfaceToLight));
+  // Specular term
+  vec3 halfVector = normalize(surfaceToCamera + normalize(surfaceToLight));
   float specularDot = dot(halfVector, surfaceNormal);
   float specularity = pow(max(specularDot, 0.0), 30);
   vec3 specular = ambientLightColor * specularity;
@@ -36,20 +37,21 @@ vec3 getAmbientLightFactor(vec3 surfacePosition, vec3 surfaceNormal) {
   return diffuse + specular;
 }
 
-vec3 getLightFactor(Light light, vec3 surfacePosition, vec3 surfaceNormal) {
-  vec3 surfaceToCamera = cameraPosition - surfacePosition;
+vec3 getLightFactor(Light light, vec3 surfacePosition, vec3 surfaceNormal, vec3 surfaceToCamera) {
   vec3 surfaceToLight = light.position - surfacePosition;
 
   float lightDistance = length(surfaceToLight);
   float attenuation = pow(1.0 / lightDistance, 2);
   vec3 lighting = light.color * light.radius * attenuation;
 
+  // Diffuse term
   vec3 normalizedSurfaceToLight = surfaceToLight / lightDistance;
   float normalDot = dot(normalizedSurfaceToLight, surfaceNormal);
   float illuminance = max(normalDot, 0.0);
   vec3 diffuse = lighting * illuminance;
 
-  vec3 halfVector = normalize(normalize(surfaceToCamera) + normalizedSurfaceToLight);
+  // Specular term
+  vec3 halfVector = normalize(surfaceToCamera + normalizedSurfaceToLight);
   float specularDot = dot(halfVector, surfaceNormal);
   float specularity = pow(max(specularDot, 0.0), 50);
   vec3 specular = lighting * specularity * 5.0;
@@ -61,13 +63,13 @@ void main() {
   vec3 albedo = texture(colorTexture, fragmentUv).xyz;
   vec3 position = texture(positionTexture, fragmentUv).xyz;
   vec4 normalDepth = texture(normalDepthTexture, fragmentUv);
+  vec3 surfaceToCamera = normalize(cameraPosition - position);
   vec3 normal = normalDepth.xyz;
-  float depth = normalDepth.w;
-  vec3 outColor = albedo * getAmbientLightFactor(position, normal);
+  vec3 combinedLighting = albedo * getAmbientLightFactor(position, normal, surfaceToCamera);
 
   for (int i = 0; i < totalLights; i++) {
-    outColor += albedo * getLightFactor(lights[i], position, normal);
+    combinedLighting += albedo * getLightFactor(lights[i], position, normal, surfaceToCamera);
   }
 
-  color = vec4(outColor, depth);
+  color = vec4(combinedLighting, normalDepth.w);
 }
