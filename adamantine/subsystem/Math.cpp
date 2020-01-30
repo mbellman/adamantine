f463990982910d6c69f3e0a952e48a4ace87f833
unknown <mbellman@nwe.com>
2020-01-29 19:24:35 -0600

    Matrix4::lookAt() fix

--- adamantine/subsystem/Math.cpp
+++ adamantine/subsystem/Math.cpp
@@ -121,8 +121,8 @@ Matrix4 Matrix4::identity() {
 
 Matrix4 Matrix4::lookAt(const Vec3f& eye, const Vec3f& direction) {
   Vec3f forward = direction.unit();
-  Vec3f right = Vec3f::crossProduct(Vec3f(0.0f, 1.0f, 0.0f), forward);
-  Vec3f up = Vec3f::crossProduct(forward, right);
+  Vec3f right = Vec3f::crossProduct(Vec3f(0.0f, 1.0f, 0.0f), forward).unit();
+  Vec3f up = Vec3f::crossProduct(forward, right).unit();
   Matrix4 translation = Matrix4::translate(eye);
 
   Matrix4 rotation = {
